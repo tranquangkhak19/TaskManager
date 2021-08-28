54c8d9dca066e29915e0c755c199cff503bf248d
tranquangkhak19 <tqk19112001@gmail.com>
2021-08-28 09:47:19 +0700

    base code

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Task Manager with PHP and MySQL</title>
+    </head>
+
+    <body>
+        <h1>TASK MANAGER</h1>
+
+        <!-- Menu Starts Here -->
+        <div class="menu">
+            <a href="index.php">Home</a>
+            <a href="#">To Do</a>
+            <a href="#">Done</a>
+        </div>
+        <!-- Menu End Here -->
+
+
+        <!-- Tasks Starts Here -->
+        <div class="all-tasks">
+            <a href="#">Add Task</a>
+            <table>
+                <tr>
+                    <th>S.N</th>
+                    <th>Task Name</th>
+                    <th>Priority</th>
+                    <th>Deadline</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td>1. </td>
+                    <td>Design a Website</td>
+                    <td>Medium</td>
+                    <td>28/08/2021</td>
+                    <td>
+                        <a href="#">Update</a>
+
+                        <a href="#">Delete</a>
+                    </td>
+
+                </tr>
+            </table>
+        </div>
+        <!-- Tasks Starts Here -->
+
+
+    </body>
+</html>
